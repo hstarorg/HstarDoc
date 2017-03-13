c48956fa7d5bf2c514e06cd2976c5281464855b8
Jay.M.Hu <hm910705@163.com>
2017-03-13 17:22:17 +0800

    set table of contents.
    Former-commit-id: 2a230a196f5acc0f7f7f5878b33320c18f1f5529
    Former-commit-id: 3f3812fd6f19dc882bfd8b69fb27eca301391894

---
+++ catalog_builder.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const replaceRegExp = /<!--TableOfContnets Start-->(\r\n|\n)*<!--TableOfContnets End-->/;
+
+const getSpaces = level => {
+  let result = '';
+  for (let i = 0; i < level; i++) {
+    result += '    ';
+  }
+  return result;
+};
+
+let tableOfContnets = '';
+
+const processDir = (folder, level = 0) => {
+  let dirs = fs.readdirSync(folder);
+  dirs.forEach(folderName => {
+    if (folderName.startsWith('.')) {
+      return;
+    }
+    let folderPath = path.join(folder, folderName);
+    if (fs.statSync(folderPath).isDirectory()) {
+      tableOfContnets += `${getSpaces(level)}* [${folderName}](${folderPath})\n`;
+      processDir(folderPath, level + 1);
+    } else if (path.extname(folderPath) === '.md') {
+      tableOfContnets += `${getSpaces(level)}* [${folderName}](${folderPath})\n`;
+    }
+  });
+};
+
+processDir('.');
+
+let readmeContent = fs.readFileSync('README.md', 'utf8');
+
+let newReadmeContent = readmeContent.replace(replaceRegExp, `<!--TableOfContnets Start-->\n${tableOfContnets}<!--TableOfContnets End-->`)
+fs.writeFileSync('README.md', newReadmeContent, 'utf8');
+
+console.log('更新目录成功！');
